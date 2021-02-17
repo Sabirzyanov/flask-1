54272fac61165e21b7daea913427ad36d78b19e5
WingiM <wingimobile@gmail.com>
2021-02-17 19:35:42 +0300

    Результат отбора

--- Code 1.py
+++ Code 1.py
@@ -224,5 +224,27 @@ def choice(planet_name):
 </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level: int, rating: float):
+    return f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossorigin="anonymous">
+    <title>Варианты выбора</title>
+</head>
+<body>
+<h1>Результаты отбора</h1>
+<h2>Претендента на участие в миссии {nickname}:</h2>
+<div class="alert alert-success">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+<div>составляет {rating}</div>
+<div class="alert alert-warning">Желаем удачи!</div>
+</body>
+</html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
